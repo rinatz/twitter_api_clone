802f70192eb6937f19d7a609123daf3670b9cb35
井田 献一朗 <ida.kenichiro@gmail.com>
2024-07-06 09:03:55 +0900

    [ADD] テストでバージョンが取得できることを確認する

--- backend/test/controllers/root_controller_test.rb
+++ backend/test/controllers/root_controller_test.rb
@@ -1,7 +1,13 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class RootControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'バージョンが取得できること' do
+    get root_url, as: :json
+    assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_includes json_response, 'apiVersion'
+  end
 end
